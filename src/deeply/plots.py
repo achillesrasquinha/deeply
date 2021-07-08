09d1ae1eea4c936ed7d2bb12f6345c23bf30847d
Achilles Rasquinha <achillesrasquinha@gmail.com>
2021-07-08 11:47:03 -0500

    fix: save plot figs

--- src/deeply/plots.py
+++ src/deeply/plots.py
@@ -13,11 +13,19 @@ def _matshow(axes, mat, title = None, axis = False, **kwargs):
     if not axis:
         axes.axis("off")
 
-def segplot(image, mask, predict = None):
+def _plot_base(fig, axes, to_file = None):
+    if to_file:
+        fig.savefig(to_file)
+
+    pplt.tight_layout()
+
+    return fig, axes
+
+def segplot(image, mask, predict = None, **kwargs):
     show_predict = predict is not None
     n_plots   = 3 if show_predict else 2
 
-    _, axes   = pplt.subplots(1, n_plots, sharex = True, sharey = True)
+    fig, axes = pplt.subplots(1, n_plots, sharex = True, sharey = True)
 
     plot_args = { "cmap": pplt.cm.gray }
 
@@ -27,15 +35,15 @@ def segplot(image, mask, predict = None):
     if show_predict:
         _matshow(axes[2], mat = predict, title = "Prediction", plot_args = plot_args)
 
-    pplt.tight_layout()
+    return _plot_base(fig, axes, **kwargs)
 
-def history_plot(history):
+def history_plot(history, **kwargs):
     histories = history.history
 
     metrics   = [m for m in iterkeys(histories) if not m.startswith("val_")]
     n_plots   = len(metrics)
 
-    _, axes   = pplt.subplots(n_plots, 1, sharex = True)
+    fig, axes = pplt.subplots(n_plots, 1, sharex = True)
 
     for i, type_ in enumerate(metrics):
         values  = histories[type_]
@@ -58,4 +66,4 @@ def history_plot(history):
 
         axes[i].legend(legends)
 
-    pplt.tight_layout()
+    return _plot_base(fig, axes, **kwargs)
