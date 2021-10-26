ec9a894863e172cfefc4ff336f873fa4ad18aa91
Achilles Rasquinha <achillesrasquinha@gmail.com>
2021-10-25 21:39:21 -0500

    feat: upload files using wandb

--- src/deeply/ops/integrations/wandb.py
+++ src/deeply/ops/integrations/wandb.py
@@ -1,11 +1,16 @@
 import os.path as osp
+from collections import Mapping
 
 from bpyutils.util.environ import setenv
+from bpyutils.util.string  import get_random_str
 
 from deeply.config import PATH
 from deeply.ops.integrations.base import BaseService
 
 _PREFIX = "WANDB"
+
+_ADD_FILE = "add_file"
+_ADD_DIR  = "add_dir"
 
 class WeightsAndBiases(BaseService):
     module = "wandb"
@@ -22,8 +27,30 @@ class WeightsAndBiases(BaseService):
 
         self._runner = module.init(project = name)
 
-    def upload(self, *files):
-        pass
+    def upload(self, *files, name = None, dest = None):
+        module   = self.module
+        name     = name or get_random_str()
+
+        artifact = module.Artifact(name, type = 'dataset')
+
+        for f in files:
+            source      = f
+            destination = dest
+
+            if isinstance(f, Mapping):
+                source      = f["source"]
+                destination = f["destination"]
+
+                module_attr = _ADD_FILE if osp.isfile(source) else _ADD_DIR
+            elif osp.isfile(f):
+                module_attr = _ADD_FILE
+            elif osp.isdir(f):
+                module_attr = _ADD_DIR
+                
+            fn = getattr(artifact, module_attr)
+            fn(source, destination)
+
+        self._runner.log_artifact(artifact)
 
     def watch(self, *models):
         module = self.module
